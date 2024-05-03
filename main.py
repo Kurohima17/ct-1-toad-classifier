84b8619376968feac59427cfd4a070217e1a4bd7
Hima Chanarithichai <ditimail@gmail.com>
2024-05-03 14:44:42 +1000

    Overhaul image ingestion workflow

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import os
 os.environ['TF_ENABLE_ONEDNN_OPTS'] = '0' # Disable OneDNN
+import numpy as np
 import tensorflow as tf
+from tensorflow.keras.preprocessing import image
 
 # Load and preprocess image
 def load_image(img_path):
@@ -13,31 +15,51 @@ def load_image(img_path):
     img_height = 256
     img_width = 256
 
-    image = tf.io.read_file(img_path) # Load image
-    image = tf.image.decode_jpeg(image, channels=3) # Decode image
-    image = tf.image.resize(image, [img_height, img_width]) # Resize image to 256x256
-    image /= 255.0  # normalize to [0,1] range
-    image = tf.expand_dims(image, 0) # Add batch dimension
-    return image
+    img = image.load_img(img_path, target_size=(img_height, img_width)) # Load image
+    img = image.img_to_array(img) # Convert image to numpy array
+    img = np.expand_dims(img, axis=0) # Add batch dimension
+    img = tf.keras.applications.xception.preprocess_input(img) # Preprocess image for Xception model
+    return img
 
 # Predict whether image is cane toad or not, extracting confidence level
-def predict_image(img_path):
+def predict_image(img_path, debug=False):
     """
     Predict whether image is cane toad or not, extracting confidence level
     :param img_path: path to image
     :return: prediction
     """
     # Load model
-    model = tf.keras.models.load_model('ct-1.keras')
+    if debug:
+        print("loading model...")
+    model = tf.keras.models.load_model('ct-1v2.keras')
 
     # Load image
+    if debug:
+        print("loading image...")
     img = load_image(img_path)
-    print(img.shape)
 
     # Make prediction
+    if debug:
+        print("making prediction...")
     prediction = model.predict(img)
+    if debug:
+        print(prediction.shape)
 
-    return prediction
+    if debug:
+        print("decoding prediction...")
+    predictionLabel = decode_prediction(prediction[0][0])
+
+    return predictionLabel
+
+def decode_prediction(prediction):
+    """
+    Decode prediction
+    :param prediction: raw Xception prediction
+    :return: JSON string 
+    """
+    is_canetoad = prediction > 0.5
+    decoded_pred = {"is_canetoad": is_canetoad, "confidence": prediction}
+    return decoded_pred
 
 # Main function
 if __name__ == "__main__":
@@ -45,14 +67,5 @@ if __name__ == "__main__":
     img_path = "data/canetoad/1.jpg"
 
     # Predict image
-    prediction = predict_image(img_path)
-    
-    # Normalise prediction to 0-1
-    prediction = tf.nn.sigmoid(prediction)
-    print(prediction)
-
-    # Print prediction
-    if prediction[0][0] > 0.5:
-        print("Cane toad")
-    else:
-        print("Not cane toad") 
+    pred = predict_image(img_path, debug=True)
+    print(pred)
